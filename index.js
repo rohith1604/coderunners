26b178aeb5dc368fb3337d19ef471fda854f8d50
Rohith T L <92637213+rohith1604@users.noreply.github.com>
2023-05-20 18:45:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("button1").addEventListener("click", function() {
+    window.location.href = "ada.html";
+  });
+  
+  document.getElementById("button2").addEventListener("click", function() {
+    window.location.href = "page2.html";
+  });
+  
+  document.getElementById("button3").addEventListener("click", function() {
+    window.location.href = "page3.html";
+  });
+  
