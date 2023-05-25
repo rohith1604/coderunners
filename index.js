0914c2e0c1da41379f81d7ceed73b137495d3aab
Rohith T L <92637213+rohith1604@users.noreply.github.com>
2023-05-25 20:44:14 +0530

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.getElementById("button1").addEventListener("click", function() {
   });
   
   document.getElementById("button2").addEventListener("click", function() {
-    window.location.href = "page2.html";
+    window.location.href = "mpl.html";
   });
   
   document.getElementById("button3").addEventListener("click", function() {
